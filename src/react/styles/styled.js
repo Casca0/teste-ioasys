c457b9435591a792b2613bf3b9f9f01cc5034a55
Lucas Borzani <lucas.borzani@enext.com.br>
2022-01-26 12:16:40 -0300

    changed background properties

--- src/react/styles/styled.js
+++ src/react/styles/styled.js
@@ -4,13 +4,13 @@ import backgroundMobile from './images/background-mobile.svg';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background: url(${image});
+    background: url(${image}) no-repeat;
+		background-position: center;
+		background-size: cover;
+		height: 100%;
 
 		@media screen and (max-width: 768px){
 			background: url(${backgroundMobile}) no-repeat;
-			background-position: center;
-			background-size: cover;
-			height: 100%;
 		}
   }
 `;
